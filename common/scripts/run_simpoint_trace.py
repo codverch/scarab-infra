eec90b63bc023fefc6629f64627210eefea2f8d4
Deepanjali Mishra <mishradeepanjali25@gmail.com>
2025-10-27 23:21:19 -0400

    Fix minimize_trace_cmd path for trace directory

--- common/scripts/run_simpoint_trace.py
+++ common/scripts/run_simpoint_trace.py
@@ -242,7 +242,7 @@ def trace_then_cluster(workload, suite, simpoint_home, bincmd, client_bincmd, si
         print("whole trace:", whole_trace)
         print("dr folder:", dr_folder)
         dr_folder =  f"{workload_home}/trace"
-        minimize_trace_cmd = f"/bin/bash /usr/local/bin/minimize_trace.sh bin {whole_trace} {workload_home}/simpoints 1 {workload_home}/traces_simp"
+        minimize_trace_cmd = f"/bin/bash /usr/local/bin/minimize_trace.sh {dr_folder}/bin {whole_trace} {workload_home}/simpoints 1 {workload_home}/traces_simp"
         subprocess.run([minimize_trace_cmd], check=True, shell=True, stdin=open(os.devnull, 'r'))
         end_time = time.perf_counter()
         report_time("minimizing traces done", start_time, end_time)
